b27ef442b7588de5e58e5e1107b78e6c1b9f8cad
ReflectFN <59999743+ReflectFN@users.noreply.github.com>
2020-07-24 19:07:39 +0200

    init
    init

--- commands/kick.js
+++ commands/kick.js
@@ -25,51 +25,7 @@ module.exports.run = async (bot, message, arguments) => {
         **Gekickt door:** ${message.author}
         **Redenen: ** ${reason}`);
 
-    var embedPrompt = new discord.MessageEmbed()
-        .setColor("GREEN")
-        .setAuthor("Gelieve te reageren binnen 30 sec.")
-        .setDescription(`Wil je ${kickUser} kicken?`);
 
-
-    message.channel.send(embedPrompt).then(async msg => {
-
-        var emoji = await promptMessage(msg, message.author, 30, ["✅", "❌"]);
-
-
-        // We kijken dat het de gebruiker is die het als eerste heeft uitgevoerd.
-        // message.channel.awaitMessages(m => m.author.id == message.author.id,
-        //     { max: 1, time: 30000 }).then(collected => {
-
-        //         if (collected.first().content.toLowerCase() == 'yes') {
-        //             message.reply('Kick speler.');
-        //         }
-        //         else
-        //             message.reply('Geanuleerd');
-
-        //     }).catch(() => {
-        //         message.reply('Geen antwoord na 30 sec, geanuleerd.');
-        //     });
-
-
-        if (emoji === "✅") {
-
-            msg.delete();
-
-            kickUser.kick(reason).catch(err => {
-                if (err) return message.channel.send(`Er is iets foutgegaan.`);
-            });
-
-            message.reply(embed);
-
-        } else if (emoji === "❌") {
-
-            msg.delete();
-
-            message.reply("Kick geanuleerd").then(m => m.delete(5000));
-
-        }
-
-    });
 }
     
 
