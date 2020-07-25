dc4118202a794b1b5ebca6724e4689fa70e0c24b
ReflectFN <59999743+ReflectFN@users.noreply.github.com>
2020-07-25 16:57:55 +0200

    init
    init

--- commands/open.js
+++ commands/open.js
@@ -1,8 +1,6 @@
 const discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-
-    message.channel.send(" @everyone ");
     
     var botEmbed = new discord.MessageEmbed()
             .setTitle('Open Zonewars Host')
