952e8dcb0910ec5d1fedba89a074ce9024b60bda
ReflectFN <59999743+ReflectFN@users.noreply.github.com>
2020-07-29 13:43:04 +0200

    init
    init

--- commands/everyone.js
+++ commands/everyone.js
@@ -3,6 +3,11 @@ const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     
     message.delete();
+
+    if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("Error");
+ 
+        if (!message.guild.me.hasPermission("KICK_MEMBERS")) return message.reply("No Perms");
+        
     return message.channel.send(" @everyone ");
     
     
