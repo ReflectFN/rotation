21dc6823fe88e7c29a25d54c60d60fd32cfdf2f7
ReflectFN <59999743+ReflectFN@users.noreply.github.com>
2020-07-29 21:19:49 +0200

    init
    init

--- commands/newgame.js
+++ commands/newgame.js
@@ -3,7 +3,7 @@ const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     
     message.delete();
-    return message.channel.send(" **New Game at 21:30!** ");
+    return message.channel.send(" **Dit was de laatste Game voor vandaag, MORGEN CUSTOMS!!** ");
     
     
         
