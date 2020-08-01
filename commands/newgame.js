be0c7cc5e31f1a1cb786a3c52aac9c0a98f8b16b
ReflectFN <59999743+ReflectFN@users.noreply.github.com>
2020-08-01 21:40:21 +0200

    init
    init

--- commands/newgame.js
+++ commands/newgame.js
@@ -3,7 +3,7 @@ const discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     
     message.delete();
-    return message.channel.send(" **Dit was de laatste game voor vandaag. Succes iedereen in de Bugha Throwback Cup! <3** ");
+    return message.channel.send(" **ee** ");
     
     
         
